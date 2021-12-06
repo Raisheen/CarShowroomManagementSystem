4e2e08b569b6606637139dd5074bddc9d1b4c268
Raisheen <59385198+Raisheen@users.noreply.github.com>
2021-12-06 17:35:21 +0600

    Add files via upload

---
+++ Database/k06.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 19, 2019 at 07:56 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `k06`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
+CREATE TABLE `cars` (
+  `carID` varchar(6) NOT NULL,
+  `model` varchar(15) NOT NULL,
+  `manufacturer` varchar(15) NOT NULL,
+  `color` varchar(15) NOT NULL,
+  `manufacturingDate` varchar(15) NOT NULL,
+  `CC` int(6) NOT NULL,
+  `price` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`carID`, `model`, `manufacturer`, `color`, `manufacturingDate`, `CC`, `price`) VALUES
+('c002', 'Civic', 'Honda', 'Black', 'March, 2018', 1800, 4100000.00),
+('c003', 'Axio', 'Toyota', 'Red', 'April, 2007', 1500, 30000000.00),
+('c005', 'Corolla', 'Toyota', 'white', 'January, 2003', 1350, 1200000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `empId` varchar(6) NOT NULL,
+  `employeeName` varchar(30) NOT NULL,
+  `designation` varchar(20) NOT NULL,
+  `salary` double(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`empId`, `employeeName`, `designation`, `salary`) VALUES
+('e00001', 'RandomEmployee1', 'Manager', 80000.00),
+('e00002', 'Mugdho', 'Salesman', 15000.00),
+('e123', 'Nobody', 'Nothing', 2323.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `userId` varchar(8) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`userId`, `password`, `status`) VALUES
+('e00001', '12345678', 0),
+('e00002', '12345678', 1),
+('e123', '11351316', 1),
+('e3302', '11223344', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+
+CREATE TABLE `purchases` (
+  `bill_no` varchar(6) NOT NULL,
+  `customerName` varchar(30) NOT NULL,
+  `customerContact` varchar(14) NOT NULL,
+  `dueDate` varchar(20) NOT NULL,
+  `carID` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `purchases`
+--
+
+INSERT INTO `purchases` (`bill_no`, `customerName`, `customerContact`, `dueDate`, `carID`) VALUES
+('b00001', 'Bill', '+8801600000000', '8/18/2019', 'c003'),
+('b00005', 'ishra', '1234656', '8/19/2019', 'c003'),
+('b00009', 'Raisheen', '1234567889', '8/19/2019', 'c006');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`carID`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`empId`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- Indexes for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD PRIMARY KEY (`bill_no`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
